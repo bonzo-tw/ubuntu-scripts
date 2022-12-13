cf67589c759c5689da24fdff2f32943e7c6dc16c
bonzo-tw <bonzo.yang@mile.cloud>
2022-12-13 01:24:55 +0000

    correct pyenv install script

--- install.sh
+++ install.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 # python
-sudo apt-get install -y python3.9
-sudo apt-get install -y python3.10
-
+sudo apt-get install curl git make
 # virtualenv
 pip3 install --no-input virtualenv
 
 # pyenv 
 curl https://pyenv.run | bash 
 
-echo '' >> ~/.bashrc
+echo '\n' >> ~/.bashrc
 echo '# added by for pyenv' >> ~/.bashrc
-echo 'export PYENV_ROOT=\"$HOME/.pyenv\"' >> ~/.bashrc
-echo 'export PATH=\"$PYENV_ROOT/bin:$PATH\"' >> ~/.bashrc
-echo 'eval \"$(pyenv init --path)\"' >> ~/.bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
 
 apt-get update; sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
-pyenv global 3.9
+# pyenv global 3.9
 
 # poetry
 curl -sSL https://install.python-poetry.org | python - --version 1.1.15
 
+echo '\n' >> ~/.bashrc
 echo '# added by for poetry' >> ~/.bashrc
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 
+# pyenv setting
+pyenv install 3.9.15
+pyenv install 3.10.9
+pyenv global 3.9.15
