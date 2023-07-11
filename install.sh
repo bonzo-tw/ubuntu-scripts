f83717622516f8d0231ef039449b871a4b03dd74
bonzo-tw <bonzo.yang@mile.cloud>
2023-07-11 10:22:50 +0000

    newer version

--- install.sh
+++ install.sh
@@ -1,30 +1,17 @@
-#!/bin/bash
-sudo apt-get install curl git make
-
-# virtualenv
-pip3 install --no-input virtualenv
-
-# pyenv 
-curl https://pyenv.run | bash 
-
-echo '' >> ~/.bashrc
-echo '# added by for pyenv' >> ~/.bashrc
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
-
-apt-get update; sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
-# poetry
-curl -sSL https://install.python-poetry.org | python - --version 1.1.15
-
-echo '' >> ~/.bashrc
-echo '# added by for poetry' >> ~/.bashrc
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
-
-# pyenv setting
-pyenv install 3.9.16
-pyenv install 3.10.9
-pyenv install 3.11.1
-pyenv global 3.9.16
+#! /bin/bash
+mkdir venv github
+sudo apt-get update
+yes | sudo apt-get install python3.9
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa
+yes | sudo apt install python3.10
+pip3 install virtualenv
+sudo fallocate -l 200G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo sysctl vm.swappiness=10
+echo '' >> /etc/sysctl.conf
+echo 'vm.swappiness=10' >> /etc/sysctl.conf
