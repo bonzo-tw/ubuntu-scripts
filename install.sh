b8cb1bb98b1720ef50a1d9cd02781e998ae48742
bonzo-tw <bonzo.yang@mile.cloud>
2024-04-27 04:58:48 +0000

    add sudo apt-get install python3-pip

--- install.sh
+++ install.sh
@@ -6,16 +6,17 @@ yes | sudo apt-get install python3.9
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa
 yes | sudo apt install python3.10
+sudo apt-get install python3-pip
 pip3 install virtualenv
-sudo fallocate -l 200G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo cp /etc/fstab /etc/fstab.bak
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-sudo sysctl vm.swappiness=10
-sudo chmod 646 /etc/sysctl.conf
-sudo echo '' >> /etc/sysctl.conf
-sudo echo 'vm.swappiness=10' >> /etc/sysctl.conf
-sudo chmod 644 /etc/sysctl.conf
-bash nfs.sh
+# sudo fallocate -l 200G /swapfile
+# sudo chmod 600 /swapfile
+# sudo mkswap /swapfile
+# sudo swapon /swapfile
+# sudo cp /etc/fstab /etc/fstab.bak
+# echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+# sudo sysctl vm.swappiness=10
+# sudo chmod 646 /etc/sysctl.conf
+# sudo echo '' >> /etc/sysctl.conf
+# sudo echo 'vm.swappiness=10' >> /etc/sysctl.conf
+# sudo chmod 644 /etc/sysctl.conf
+# bash nfs.sh
